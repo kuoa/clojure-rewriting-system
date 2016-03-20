30b4ce80b275a9aecf77476acd82dc346e446442
kuoa <postarusandu@gmail.com>
2016-03-20 14:33:21 +0100

    Add rules

---
+++ src/clojure_rewriting_system/rules.clj
@@ -0,0 +1,52 @@
+(ns clojure-rewriting-system.rules
+  (:require [clojure-rewriting-system.terms :refer :all]))
+
+(defn variables
+  "Returns a set of all the variables in `t`.
+  Terminal using a dummy stack."
+  [t]
+  (loop [t t, stack '() rez #{}]
+    (cond
+      (variable? t) (if (seq stack)
+                      (recur (first stack) (rest stack) (conj rez t))
+                      (conj rez t))
+      (or
+       (seq-term? t)
+       (vec-term? t)) (if (seq t) ;; '() | [] -> true
+                        (recur (first t) (conj stack (rest t)) rez)
+                        (if (seq stack)                        
+                          (recur (first stack) (rest stack) rez)
+                          rez)) 
+
+      (map-term? t) (if (seq t) ;; {} -> true                      
+                      (recur (second (first t))
+                             (conj stack (dissoc t (first (first t)))) rez)
+                      (if (seq stack)
+                        (recur (first stack) (rest stack) rez)
+                        rez))
+
+    :else (if (seq stack)
+              (recur (first stack) (rest stack) rez)
+              rez))))
+
+(defn variables-subset?
+  "Check if the variables are a subset"
+  [[l r]]
+  (clojure.set/subset? (variables r) (variables l)))
+
+(defn rule?
+  "Check if the rule `[left right]` has the correct form"
+  [t]
+  (and (vector? t)
+       (= (count t) 2)
+       (term? (first t))
+       (term? (second t))
+       (variables-subset? t)))
+
+(defn l
+  "Left side rule access"
+  [t] (first t))
+
+(defn r
+  "Right side rule access"
+  [t] (second t))
---
+++ test/clojure_rewriting_system/rules_test.clj
@@ -0,0 +1,26 @@
+(ns clojure-rewriting-system.test_rules
+  (:use midje.sweet)
+  (:require [clojure-rewriting-system.rules :refer :all]))
+
+(fact "Rules"
+      (rule? '[(?X + 1) ?X]) => true)
+
+(fact "Not valid rules"
+      (rule? '[(?X + 1)]) => false
+      (rule? '[?X nil]) => false
+      (rule? '[(?X * 12) (?X + ?Y)]) => false
+      (rule? '[{:x ?X :y {:z ?Z}} ?Y]) => false)
+
+(fact "Right / left rule access."
+     (l '[(?X + 1) ?X]) => '(?X + 1)
+     (r '[(?X + 1) ?X]) => '?X)
+
+
+(fact "Variables"
+      (variables '?X) => '#{?X}
+      (variables '(?X ?Y ?Y)) => '#{?X ?Y}
+      (variables '(?X (?Y (?X ?Y ?Z hey)))) => '#{?X ?Y ?Z}
+      (variables 'hey) => '#{}
+      (variables '[?X [?Y]]) => '#{?X ?Y}
+      (variables '{:x ?X :y ?Y :b ?Z}) => '#{?X ?Y ?Z}
+      (variables '({:x ?X :y {:z ?Z}} [(?Y {:a ?A})])) => '#{?X ?Z ?Y ?A})
